80182555744ecdf259a01bf1be475a430ca67f0a
ShreyasNimkar <nimkarshreyas22@gmail.com>
2023-05-21 23:20:28 +0530

    feat: initialized google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import ReactGA from 'react-ga';
+
+const trackingID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID;
+
+if (trackingID) {
+    ReactGA.initialize(trackingID);
+} else {
+    // Handle the case when the tracking ID is undefined
+    console.error('Google Analytics tracking ID is not defined.');
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,13 @@ import Header from '@/components/common/header';
 import EventsSection from '@/components/sections/landing-page-sections/eventsSection';
 import PatronsSection from '@/components/sections/landing-page-sections/patronsSection';
 import TimelineSection from '@/components/sections/landing-page-sections/timelineSection';
-
+import ReactGA from 'react-ga';
+import { useEffect, useState } from 'react';
 export default function Home() {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname);
+    }, []);
+
     return (
         <>
             <Header />
