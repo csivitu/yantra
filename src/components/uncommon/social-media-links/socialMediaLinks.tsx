198b9bac88f6e0226b46929a908153bf770eb165
Pratham_Mishra04 <prathamm0409@gmail.com>
2023-06-01 04:27:11 +0530

    feat: side links edited

--- src/components/uncommon/social-media-links/socialMediaLinks.tsx
+++ src/components/uncommon/social-media-links/socialMediaLinks.tsx
@@ -8,32 +8,31 @@ const SocialMediaLinks = () => {
     const handles = ['youtube', 'twitter', 'instagram', 'linkedIn'];
 
     return (
-        <div className="w-1/2 flex justify-around items-center m-auto">
+        <div className="h-[75%] flex flex-col justify-around items-end m-auto max-md:flex-row max-md:w-full max-md:items-center max-md:justify-center">
             {handles.map((handle: string, index: number) => {
                 return (
                     <div
                         key={index}
                         className="flex justify-center items-center ml-16"
-                        onMouseEnter={() => setToggle(index)}
-                        onMouseLeave={() => setToggle(-1)}
                     >
+                        <SlideInLinkText
+                            isVisible={toggle}
+                            children={
+                                <div className="text-lg uppercase font-spaceGrotesk text-white font-semibold max-md:hidden">
+                                    {handle}
+                                </div>
+                            }
+                            xIndex={index === 0 ? -10 : 0}
+                            elIndex={index}
+                        />
                         <Image
                             width={10000}
                             height={10000}
                             src={`/${handle}.png`}
                             alt="/"
                             className="w-12 h-8 object-contain z-50 cursor-pointer"
-                        />
-
-                        <SlideInLinkText
-                            isVisible={toggle}
-                            children={
-                                <div className="text-lg uppercase font-spaceGrotesk text-white font-semibold">
-                                    {handle}
-                                </div>
-                            }
-                            xIndex={index === 0 ? 10 : 0}
-                            elIndex={index}
+                            onMouseEnter={() => setToggle(index)}
+                            onMouseLeave={() => setToggle(-1)}
                         />
                     </div>
                 );
--- src/sections/landing-page-sections/sponsorsSection.tsx
+++ src/sections/landing-page-sections/sponsorsSection.tsx
@@ -7,19 +7,20 @@ const SponsorsSection = () => {
                 id="sponsors-section"
                 className="w-fit font-spaceGrotesk uppercase text-8xl font-extrabold m-auto pt-40 pb-20"
             >
-            <div className="w-fit text-white font-bronson uppercase sm:text-8xl font-extrabold m-auto pt-40 pb-20">
-                Sponsors
-            </div>
-            <div className="mb-20">
-                <div className="h-1/3 flex justify-around items-center">
-                    top2
+                <div className="w-fit text-white font-bronson uppercase sm:text-8xl font-extrabold m-auto pt-40 pb-20">
+                    Sponsors
                 </div>
+                <div className="mb-20">
+                    <div className="h-1/3 flex justify-around items-center">
+                        top2
+                    </div>
 
-                <div className="h-1/3 flex justify-around items-center">
-                    4here
-                </div>
-                <div className="h-1/3 flex justify-around items-center">
-                    4here
+                    <div className="h-1/3 flex justify-around items-center">
+                        4here
+                    </div>
+                    <div className="h-1/3 flex justify-around items-center">
+                        4here
+                    </div>
                 </div>
             </div>
         </>
