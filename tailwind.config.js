1338fd84aefd03a3e6a759d80db5d711c04c2e01
Pratham_Mishra04 <prathamm0409@gmail.com>
2023-05-31 06:49:57 +0530

    feat: timeline added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                spaceGrotesk: ['Space Grotesk'],
+            },
+        },
+    },
     plugins: [],
 };
