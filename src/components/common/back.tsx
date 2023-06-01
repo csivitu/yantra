f3656978d580f2d6af3517b7ebbd58be426c90a6
Pratham_Mishra04 <prathamm0409@gmail.com>
2023-06-01 07:33:23 +0530

    feat: back button added

---
+++ src/components/common/back.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const Back = () => {
+    const router = useRouter();
+    return (
+        <svg
+            viewBox="0 0 1024 1024"
+            fill="#ffffff"
+            className="icon cursor-pointer"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#ffffff"
+            width="75"
+            height="75"
+            onClick={() => router.back()}
+        >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+                <path
+                    d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z"
+                    fill=""
+                ></path>
+            </g>
+        </svg>
+    );
+};
+
+export default Back;
