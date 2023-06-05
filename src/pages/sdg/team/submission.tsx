4195e2c3676f87695b2db1cbbac05d1a1aab2d07
ShreyasNimkar <nimkarshreyas22@gmail.com>
2023-06-06 01:28:07 +0530

    fix:file structuring

--- src/pages/sdg/team/submission.tsx
+++ src/pages/sdg/team/submission.tsx
@@ -2,7 +2,8 @@ import Footer from '@/components/common/footer';
 import Header from '@/components/common/header';
 import HeroSection from '@/sections/project-submission-page-sections/HeroSection';
 import React from 'react';
-
+import { GetServerSidePropsContext } from 'next';
+import { getSession } from 'next-auth/react';
 const ProjectSubmission = () => {
     return (
         <>
@@ -14,5 +15,19 @@ const ProjectSubmission = () => {
         </>
     );
 };
-
+export const getServerSideProps = async (
+    context: GetServerSidePropsContext
+) => {
+    const session = await getSession(context);
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/sdg',
+            },
+        };
+    }
+    return {
+        props: {},
+    };
+};
 export default ProjectSubmission;
