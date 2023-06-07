d029e12e5c8ce6b8bd1a3d4214bb73ebb3f60186
Pratham_Mishra04 <prathamm0409@gmail.com>
2023-06-07 21:21:43 +0530

    feat: minor ui fixes

--- src/components/uncommon/sdg-admin-page-cards/ProjectCard.tsx
+++ src/components/uncommon/sdg-admin-page-cards/ProjectCard.tsx
@@ -22,12 +22,12 @@ const ProjectCard = ({ team }: Props) => {
                 href={`/hack/admin/team/${team._id}`}
                 className={`lg:w-[20vw] w-[40%] h-[25vh] font-spaceGrotesk glassMorphism transition-all duration-100 ease-in-out ${
                     hover ? 'content2 ' : ''
-                }  rounded-lg border-[2px] border-white lg:h-[35vh] py-4 flex flex-col sm:flex-row justify-around items-center text-white`}
+                }   rounded-lg border-[2px] border-white lg:h-[35vh] py-4 flex flex-col sm:flex-row justify-around items-center text-3xl max-sm:line-clamp-1 max-sm:text-base text-white`}
             >
                 {/* <div className="h-full text-lg text-center py-2 sm:py-3 w-full sm:w-[20%] flex justify-center items-center">
                     Project Name: {team.submission.title}
                 </div> */}
-                <div className="h-full capitalize text-3xl text-center py-2 sm:py-3 w-full sm:w-[20%] flex justify-center items-center">
+                <div className="h-full capitalize text-center py-2 sm:py-3 w-full sm:w-[20%] flex justify-center items-center">
                     {team.title}
                 </div>
                 {/* <div className="h-full text-lg py-2 sm:py-3 w-full sm:w-[20%] flex justify-center items-center">
